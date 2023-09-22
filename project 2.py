8d1e039aa8ccf99809228a41e57cf6954079303b
Pramdha <145755265+Pramdha@users.noreply.github.com>
2023-09-22 22:16:31 +0530

    Add files via upload

---
+++ project 2.py
@@ -0,0 +1,3 @@
+filename = input("Input the filename:abc.py The extension of the file is:'python' ")
+f_extns = filename.split("python")
+print ( "The extension of the file is : " + repr(f_extns[-1]))
